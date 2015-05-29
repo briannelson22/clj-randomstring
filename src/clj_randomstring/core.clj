2f894b4f0eea9679e5d995a8601d5c417a3c44f4
Ubuntu <ubuntu@ip-172-31-26-198.us-west-2.compute.internal>
2015-05-29 01:47:01 +0000

    First working version of string generation function

--- src/clj_randomstring/core.clj
+++ src/clj_randomstring/core.clj
@@ -1,6 +1,61 @@
-(ns clj-randomstring.core)
+(ns clj-randomstring.core
+	(:require [clj-time.core :as t]
+		[clj-time.format :as f]))
 
-(defn foo
+(defn hello-shared 
   "I don't do a whole lot."
   [x]
-  (println x "Hello, World!"))
+  (str x " Hello, World!"))
+
+(defn hello 
+  "I don't do a whole lot."
+  [x]
+  (println (hello-shared x)))
+
+(defn handler-hello
+	"My Sample http request handler"
+	[request]
+	{
+	:status 200
+	:headers {"Content-Type" "text/html"}
+	:body (hello-shared "Kelly")})
+
+(defn handler-remote-ip
+	"Handler that returns the requests ip address"
+	[request]
+	{
+	:status 200
+	:headers {"Content-Type" "text/plain"}
+	:body (:remote-addr request)})	
+
+(defn num-to-month
+	[num]
+	(subs (f/unparse (f/formatter "MMM") (t/date-time 1970 num 01)) 0 1))
+
+(defn part1
+	[in-date]
+	(num-to-month (mod (+ (t/month in-date) (t/hour in-date)) 12)))
+
+(defn part2
+	[in-date]
+	(+ (t/day in-date) (t/minute in-date) -9))
+
+(defn part3
+	[in-date]
+	(let [result (+ (t/year in-date) (t/second in-date) -2011)]
+		(str
+			(cond
+				(< result 0) "N"
+				(> result 0) "P"
+				:else "Z")
+			result
+		)))
+	
+	
+(defn random-string
+	"Generate a random string"
+	[in-date]
+	(str 
+		(part1 in-date)
+		(part2 in-date)
+		(part3 in-date)))
--- test/clj_randomstring/core_test.clj
+++ test/clj_randomstring/core_test.clj
@@ -2,6 +2,18 @@
   (:require [clojure.test :refer :all]
             [clj-randomstring.core :refer :all]))
 
-(deftest a-test
-  (testing "FIXME, I fail."
-    (is (= 0 1))))
+(deftest part1
+  (testing "part1"
+    (is (= "J" (part1 (t/date-time 2015 01 01 05 25 35))))))
+
+(deftest part2
+  (testing "part2"
+    (is (= "17" (part2 (t/date-time 2015 01 01 05 25 35))))))
+
+(deftest part3
+  (testing "part3"
+    (is (= "P39" (part3 (t/date-time 2015 01 01 05 25 35))))))
+ 
+(deftest random-date
+  (testing "random-date"
+    (is (= "J17P39" (random-string "20150101" "052535"))))
